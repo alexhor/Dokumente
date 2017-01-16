84d55ba26d672a8f047f0f390a94a7ee712a8f2c
Alexander Hornig <alexander@h-software.de>
2017-01-16 15:20:49 +0100

    Hangman (#9)
    * added project description
    
    * adding example file for using clear function
    
    * added clear module
    
    * added main function from lession

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,6 +1,53 @@
 # import the clear function
 from lib.clear import clear
 
+def main():
+    """main function that runs the whole game
+
+    no tests here, because there is nothing to test here"""
+    word = input("Wort zum Erraten eingeben: ")
+    word = word.upper(word)
+    print_underscores(word)
+    # create variables
+    wrong_letters = []
+    guessed = []
+    # main game loop
+    while True:
+        letter = get_letter()
+        if test_letter(letter, word):
+            guessed = replace_letter(letter, word, guessed)
+        else:
+            wrong_letters.append(letter)
+            print_hangman(wrong_letters)
+        if word_complete(guessed):
+            break;
+    # the game has been won
+    print("You won!")
+
+def print_underscores(word):
+    """TODO: fill with your code"""
+    pass
+
+def get_letter():
+    """TODO: fill with your code"""
+    pass
+
+def test_letter(letter, word):
+    """TODO: fill with your code"""
+    pass
+
+def replace_letter(letter, word, guessed):
+    """TODO: fill with your code"""
+    pass
+
+def print_hangman(wrong_words):
+    """TODO: fill with your code"""
+    pass
+
+def word_complete(guessed):
+    """TODO: fill with your code"""
+    pass
+
 def loading_screen():
     """example for using the function to refresh the screen
 
