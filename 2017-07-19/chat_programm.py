a1aadbef6cb1d138b2f92f755ebe44f5200aa1c6
Robin Grether <privat@robingrether.de>
2017-07-19 15:07:48 +0200

    Verwende PyQt4 und füge Launch-Fenster hinzu.

--- 2017-07-19/chat_programm.py
+++ 2017-07-19/chat_programm.py
@@ -1,5 +1,5 @@
 import json, rotation_encryption, simple_socket, sys, threading, time
-from PyQt5 import QtWidgets
+from PyQt4.QtGui import *
 
 class Packet:
 
@@ -65,72 +65,101 @@ class Packet:
 #    print(packet2.sender_name + (" hat sich ausgeloggt" if packet2.aktion == "logout" else " hat sich eingeloggt"))
 
 
-class ChatProgram(QtWidgets.QWidget):
+class ChatProgram(QWidget):
 
     def __init__(self):
+        super().__init__()
         self.__running = False
         self.__nickname = None
         self.__my_socket = None
         self.__user_input_thread = None
         self.__socket_input_thread = None
 
-    def launch(self):
-        print("------ Chat-Programm ------\n\nServer-Adresse: ", end='')
-        server_address = input()
-        while True:
-            try:
-                print("Server-Port: ", end='')
-                server_port = int(input())
-                if server_port >= 0 and server_port <= 65535:
-                    break
-                else:
-                    print("Ungültige Eingabe!")
-            except:
-                print("Ungültige Eingabe!")
 
-        print("\nNickname: ", end='')
+    def init_launch_ui(self):
+        self.setWindowTitle("Launch Chat")
 
-        self.__nickname = input()
+        v_box = QVBoxLayout()
+        self.setLayout(v_box)
 
-        print("\nVerbinde...")
+        self.label1 = QLabel("Server-Adresse: ")
+        self.line1 = QLineEdit()
+        h_box = QHBoxLayout()
+        h_box.addWidget(self.label1)
+        h_box.addWidget(self.line1)
+        v_box.addLayout(h_box)
+
+        self.label2 = QLabel("Server-Port: ")
+        self.line2 = QLineEdit()
+        h_box = QHBoxLayout()
+        h_box.addWidget(self.label2)
+        h_box.addWidget(self.line2)
+        v_box.addLayout(h_box)
         
+        self.label3 = QLabel("Username: ")
+        self.line3 = QLineEdit()
+        h_box = QHBoxLayout()
+        h_box.addWidget(self.label3)
+        h_box.addWidget(self.line3)
+        v_box.addLayout(h_box)
+
+        self.button_login = QPushButton("Verbinden")
+        self.button_login.clicked.connect(self.connect)
+        v_box.addWidget(self.button_login)
+
+        self.show()
+        
+
+    def connect(self):
+        server_address = self.line1.text()
+        server_port = self.line2.text()
+
+        try:
+            server_port = int(str(server_port))
+        except:
+            return
+
         self.__my_socket = simple_socket.connect_as_client(server_address, server_port)
 
         if not self.__my_socket:
-            print("\nVerbindungsaufbau fehlgeschlagen...")
             return
-        
+
+        self.__nickname = self.line3.text()
+
         self.__my_socket.send(self.create_packet_2(self.__nickname, "login"))
         
         self.__socket_input_thread = threading.Thread(target=self.socket_input_runnable, args=())
 
-        print("\nEingeloggt!\n")
-
-        self.init_ui()
+        self.init_chat_ui()
 
         self.__running = True
 
         self.__socket_input_thread.start()
 
 
-    def init_ui(self):
-        super().__init__()
+    def init_chat_ui(self):
+
+        while self.layout().count():
+            child = self.layout().takeAt(0)
+            if child.widget():
+                child.widget().deleteLater()
+        
         self.setWindowTitle("Chat")
 
-        self.chat = QtWidgets.QTextEdit()
+        self.chat = QTextEdit()
         self.chat.setReadOnly(True)
 
-        self.line = QtWidgets.QLineEdit()
+        self.line = QLineEdit()
         self.line.returnPressed.connect(self.button_clicked)
 
-        self.button = QtWidgets.QPushButton("Senden")
+        self.button = QPushButton("Senden")
         self.button.clicked.connect(self.button_clicked)
 
-        h_box = QtWidgets.QHBoxLayout()
+        h_box = QHBoxLayout()
         h_box.addWidget(self.line)
         h_box.addWidget(self.button)
 
-        v_box = QtWidgets.QVBoxLayout()
+        v_box = self.layout()
         v_box.addWidget(self.chat)
         v_box.addLayout(h_box)
 
@@ -187,8 +216,7 @@ class ChatProgram(QtWidgets.QWidget):
                 
 
 
-app = QtWidgets.QApplication(sys.argv)
+app = QApplication(sys.argv)
 chat_program = ChatProgram()
-chat_program.launch()
-chat_program.show()
+chat_program.init_launch_ui()
 sys.exit(app.exec_())
